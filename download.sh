f59f7af98a38f665f208891862372471cba49f5b
kklein <kleinkevin.lu@gmail.com>
2022-08-31 09:16:24 +0200

    Add script to download data from causality book.

---
+++ download.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir data
+cd data
+wget https://raw.githubusercontent.com/matheusfacure/python-causality-handbook/master/causal-inference-for-the-brave-and-true/data/learning_mindset.csv
+cd ..
