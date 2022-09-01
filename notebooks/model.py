187b156c06749859cdf3abad1017f99ca3d21bcf
kklein <kleinkevin.lu@gmail.com>
2022-09-01 11:20:31 +0200

    Move notebooks to separate folder.

--- notebooks/model.py
+++ notebooks/model.py
@@ -4,7 +4,7 @@ from xgboost import XGBRegressor
 
 
 def get_model():
-    df = pd.read_csv("./data/learning_mindset.csv")
+    df = pd.read_csv("../data/learning_mindset.csv")
 
     outcome_column = "achievement_score"
     treatment_column = "intervention"
@@ -13,10 +13,12 @@ def get_model():
         for column in df.columns
         if column not in [outcome_column, treatment_column]
     ]
-    
+
     r_learner = BaseRRegressor(learner=XGBRegressor(random_state=42))
     r_learner.fit(
         X=df[feature_columns], treatment=df[treatment_column], y=df[outcome_column]
     )
     tau = r_learner.models_tau[1]
     return tau, df, feature_columns
+
+get_model()
--- readme.md
+++ readme.md
@@ -1,7 +1,17 @@
-Workbench for illustrating the use of different explainability approaches on a CATE model.
+Workbench for illustrating different explainability approaches on a CATE model.
 
-```
+## Notebooks
+
+Some notebooks using popular tooling are provided. To get started, run:
+
+```console
 $ mamba env create -f environment.yml
 $ conda activate explain
 $ ./download.sh
+$ cd notebooks
+$ jupyter notebook
 ```
+
+## Methodological summaries
+
+* [LIME]()
